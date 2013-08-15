7ddd978bed7d098006a1145984ca1de50e78119c
benzhemin <benzhemin@gmail.com>
2013-08-15 22:56:55 +0800

    完成注册UI

--- PosMini_Iphone/ui/Controller/login/RegisterViewController.h
+++ PosMini_Iphone/ui/Controller/login/RegisterViewController.h
@@ -7,7 +7,81 @@
 //
 
 #import "BaseViewController.h"
+#import "RegTextField.h"
 
-@interface RegisterViewController : BaseViewController
+
+@interface RegisterViewController : BaseViewController<UITextFieldDelegate>{
+    //背景ScrollView
+    UIScrollView *bgScrollView;
+    
+    //手机号码
+    RegTextField *phoneNumTextField;
+    //图片验证码
+    RegTextField *imageSafeCodeTextField;
+    //短信验证码
+    RegTextField *phoneMessageSafeCodeTextField;
+    //姓名
+    RegTextField *usernameTextField;
+    //身份证号
+    RegTextField *userIdentifierTextField;
+    //密码
+    RegTextField *pwdTextField;
+    //确认密码
+    RegTextField *confirmPwdTextField;
+    
+    //图片验证码
+    UIButton *imageSafeCodeButton;
+    //发送短信按钮
+    UIButton *sendMessageButton;
+    //注册按钮
+    UIButton *registerButton;
+    
+    //获取焦点文本输入框
+    UITextField *focusTextField;
+    
+    //是否正在获取图片验证码
+    Boolean isLoadingSafeCodeImage;
+    
+    //存储随机图片验证码种子
+    NSMutableString *safeCodeRandomString;
+    //是否已经获取短信验证码
+    Boolean isGetMessageCode;
+    //是否是第一次获取短信验证码
+    Boolean isFirstGetMessageCode;
+    //显示倒计时
+    UILabel *timerLabel;
+    //Timer计数器
+    int timerCount;
+}
+
+@property (nonatomic, retain) UIScrollView *bgScrollView;
+
+@property (nonatomic, retain) RegTextField *phoneNumTextField;
+@property (nonatomic, retain) RegTextField *imageSafeCodeTextField;
+@property (nonatomic, retain) UIButton *imageSafeCodeButton;
+@property (nonatomic, retain) RegTextField *phoneMessageSafeCodeTextField;
+@property (nonatomic, retain) UIButton *sendMessageButton;
+@property (nonatomic, retain) UILabel *timerLabel;
+
+@property (nonatomic, retain) RegTextField *usernameTextField;
+@property (nonatomic, retain) RegTextField *userIdentifierTextField;
+@property (nonatomic, retain) RegTextField *pwdTextField;
+@property (nonatomic, retain) RegTextField *confirmPwdTextField;
+@property (nonatomic, retain) UIButton *registerButton;
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PosMini_Iphone/ui/Controller/login/RegisterViewController.m
+++ PosMini_Iphone/ui/Controller/login/RegisterViewController.m
@@ -9,30 +9,219 @@
 #import "RegisterViewController.h"
 
 @interface RegisterViewController ()
+//产生随机8位数作为图片验证码种子
+-(NSString *)createRandomString;
 
 @end
 
 @implementation RegisterViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+@synthesize bgScrollView;
+
+@synthesize phoneNumTextField, imageSafeCodeTextField;
+@synthesize imageSafeCodeButton;
+@synthesize phoneMessageSafeCodeTextField;
+@synthesize sendMessageButton;
+@synthesize timerLabel;
+
+@synthesize usernameTextField;
+@synthesize userIdentifierTextField;
+@synthesize pwdTextField;
+@synthesize confirmPwdTextField;
+@synthesize registerButton;
+
+-(void)dealloc{
+    [bgScrollView release];
+    
+    [phoneNumTextField release];
+    [imageSafeCodeTextField release];
+    [imageSafeCodeButton release];
+    
+    [phoneMessageSafeCodeTextField release];
+    [sendMessageButton release];
+    
+    [timerLabel release];
+    
+    [usernameTextField release];
+    [userIdentifierTextField release];
+    [pwdTextField release];
+    [confirmPwdTextField release];
+    
+    [registerButton release];
+    
+    [super dealloc];
 }
+
+
+#define MARGIN_LEFT 15
+#define INPUTTEXTFILED_HEIGHT 45
+#define LINE_MARGIN 5
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	
+    [self setNavigationTitle:@"注册"];
+    
+    //背景ScrollView
+    CGRect bgFrame = CGRectMake(0, 0, contentView.frame.size.width, contentView.frame.size.height);
+    self.bgScrollView = [[[UIScrollView alloc]initWithFrame:bgFrame] autorelease];
+    [contentView addSubview:bgScrollView];
+    
+    //起始位置y坐标
+    float _begY = 10;
+    //手机号码
+    self.phoneNumTextField = [[[RegTextField alloc] initWithFrame:CGRectMake(MARGIN_LEFT,_begY, contentView.frame.size.width-2*MARGIN_LEFT, INPUTTEXTFILED_HEIGHT)] autorelease];
+    [phoneNumTextField setTitle:@"手机号码:"];
+    [phoneNumTextField setPlaceholder:@"请输入手机号码"];
+    [phoneNumTextField setReturnKeyType:UIReturnKeyNext];
+    [phoneNumTextField setKeyBoardType:UIKeyboardTypeNumberPad];
+    [phoneNumTextField setFieldTag:1];
+    [phoneNumTextField setDelegate:self];
+    [bgScrollView addSubview:phoneNumTextField];
+    
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+    
+    //图片验证码输入框
+    self.imageSafeCodeTextField = [[[RegTextField alloc]initWithFrame:CGRectMake(MARGIN_LEFT,_begY, 110, INPUTTEXTFILED_HEIGHT)] autorelease];
+    [imageSafeCodeTextField setDelegate:self];
+    [imageSafeCodeTextField setReturnKeyType:UIReturnKeyNext];
+    [imageSafeCodeTextField setPlaceholder:@"图片验证码"];
+    [imageSafeCodeTextField setFieldTag:2];
+    [bgScrollView addSubview:imageSafeCodeTextField];
+
+    //显示图片验证码
+    self.imageSafeCodeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    imageSafeCodeButton.tag = 1;
+    [imageSafeCodeButton addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [imageSafeCodeButton setBackgroundColor:[UIColor whiteColor]];
+    [imageSafeCodeButton setTitle:@"点击获取验证码" forState:UIControlStateNormal];
+    [imageSafeCodeButton setTitleColor:[UIColor colorWithRed:57/255.0 green:84/255.0 blue:134/255.0 alpha:1.0] forState:UIControlStateNormal];
+    imageSafeCodeButton.frame = CGRectMake(imageSafeCodeTextField.frame.origin.x+imageSafeCodeTextField.frame.size.width+5, _begY, contentView.frame.size.width-2*MARGIN_LEFT-imageSafeCodeTextField.frame.size.width-8, INPUTTEXTFILED_HEIGHT);
+    [bgScrollView addSubview:imageSafeCodeButton];
+    
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+
+    //短信验证码输入框
+    self.phoneMessageSafeCodeTextField = [[[RegTextField alloc]initWithFrame:CGRectMake(MARGIN_LEFT,_begY, 110, INPUTTEXTFILED_HEIGHT)] autorelease];
+    [phoneMessageSafeCodeTextField setDelegate:self];
+    [phoneMessageSafeCodeTextField setFieldTag:3];
+    [phoneMessageSafeCodeTextField setReturnKeyType:UIReturnKeyNext];
+    [phoneMessageSafeCodeTextField setKeyBoardType:UIKeyboardTypeNumberPad];
+    [phoneMessageSafeCodeTextField setPlaceholder:@"短信验证码"];
+    [bgScrollView addSubview:phoneMessageSafeCodeTextField];
+    
+    //发送短信验证码按钮
+    self.sendMessageButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [sendMessageButton addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    sendMessageButton.tag = 2;
+    sendMessageButton.frame = CGRectMake(phoneMessageSafeCodeTextField.frame.origin.x+phoneMessageSafeCodeTextField.frame.size.width+5, _begY, contentView.frame.size.width-2*MARGIN_LEFT-phoneMessageSafeCodeTextField.frame.size.width-5, INPUTTEXTFILED_HEIGHT);
+    [sendMessageButton setBackgroundImage:[[UIImage imageNamed:@"code-bg.png"]stretchableImageWithLeftCapWidth:10 topCapHeight:0] forState:UIControlStateNormal];
+    [sendMessageButton setTitle:@"点击获取短信验证码" forState:UIControlStateNormal];
+    [sendMessageButton setTitleColor:[UIColor colorWithRed:57/255.0 green:84/255.0 blue:134/255.0 alpha:1.0] forState:UIControlStateNormal];
+    sendMessageButton.titleLabel.font = [UIFont systemFontOfSize:14];
+    [bgScrollView addSubview:sendMessageButton];
+    
+    //显示倒计时
+    self.timerLabel = [[[UILabel alloc] init] autorelease];
+    timerLabel.hidden = YES;
+    timerLabel.font = [UIFont systemFontOfSize:16];
+    timerLabel.textColor = [UIColor grayColor];
+    timerLabel.backgroundColor = [UIColor clearColor];
+    timerLabel.frame = CGRectMake(sendMessageButton.frame.size.width-35, 4, 20, 35);
+    [sendMessageButton addSubview:timerLabel];
+    
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+    
+    //姓名
+    self.usernameTextField = [[[RegTextField alloc]initWithFrame:CGRectMake(MARGIN_LEFT,_begY, contentView.frame.size.width-2*MARGIN_LEFT, INPUTTEXTFILED_HEIGHT)] autorelease];
+    [usernameTextField setDelegate:self];
+    [usernameTextField setReturnKeyType:UIReturnKeyNext];
+    [usernameTextField setFieldTag:4];
+    [usernameTextField setTitle:@"姓名:"];
+    [usernameTextField setPlaceholder:@"请输入姓名"];
+    [bgScrollView addSubview:usernameTextField];
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+    
+    //身份证
+    self.userIdentifierTextField = [[RegTextField alloc]initWithFrame:CGRectMake(MARGIN_LEFT,_begY, contentView.frame.size.width-2*MARGIN_LEFT, INPUTTEXTFILED_HEIGHT)];
+    [userIdentifierTextField setDelegate:self];
+    [userIdentifierTextField setReturnKeyType:UIReturnKeyNext];
+    //[userIdentifierTextField setKeyBoardType:UIKeyboardTypeNumberPad];
+    [userIdentifierTextField setFieldTag:5];
+    [userIdentifierTextField setTitle:@"身份证号码:"];
+    [userIdentifierTextField setPlaceholder:@"请输入18位身份证号码"];
+    [bgScrollView addSubview:userIdentifierTextField];
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+    
+    //密码
+    self.pwdTextField = [[[RegTextField alloc]initWithFrame:CGRectMake(MARGIN_LEFT,_begY, contentView.frame.size.width-2*MARGIN_LEFT, INPUTTEXTFILED_HEIGHT)] autorelease];
+    [pwdTextField setDelegate:self];
+    [pwdTextField setReturnKeyType:UIReturnKeyNext];
+    [pwdTextField setFieldTag:6];
+    [pwdTextField setSecureTextEntry:YES];
+    [pwdTextField setTitle:@"密码:"];
+    [pwdTextField setPlaceholder:@"6到12位的数字或字母"];
+    [bgScrollView addSubview:pwdTextField];
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+    
+    //确认密码
+    self.confirmPwdTextField = [[[RegTextField alloc]initWithFrame:CGRectMake(MARGIN_LEFT,_begY, contentView.frame.size.width-2*MARGIN_LEFT, INPUTTEXTFILED_HEIGHT)] autorelease];
+    [confirmPwdTextField setDelegate:self];
+    [confirmPwdTextField setSecureTextEntry:YES];
+    [confirmPwdTextField setReturnKeyType:UIReturnKeyDone];
+    [confirmPwdTextField setFieldTag:7];
+    [confirmPwdTextField setTitle:@"密码确认:"];
+    [confirmPwdTextField setPlaceholder:@"请再次输入密码"];
+    [bgScrollView addSubview:confirmPwdTextField];
+    _begY+=INPUTTEXTFILED_HEIGHT;
+    _begY+=LINE_MARGIN;
+    
+    //注册按钮
+    _begY+=3;
+    self.registerButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [registerButton addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    registerButton.frame = CGRectMake(MARGIN_LEFT, _begY, contentView.frame.size.width-2*MARGIN_LEFT, 47);
+    registerButton.tag = 3;
+    registerButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [registerButton setTitle:@"注        册" forState:UIControlStateNormal];
+    [registerButton setBackgroundImage:[[UIImage imageNamed:@"reg-btn.png"] stretchableImageWithLeftCapWidth:10 topCapHeight:0] forState:UIControlStateNormal];
+    [bgScrollView addSubview:registerButton];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSString *)createRandomString{
+    
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
