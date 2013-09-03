5f927bff09f747659cb1ca45baf0c2d6aa111286
benzhemin <benzhemin@gmail.com>
2013-09-03 10:54:42 +0800

    防止popViewController时,设置错误的baseCTRL

--- PosMini_Iphone/ui/Custom/Controller/CPNavigationController.h
+++ PosMini_Iphone/ui/Custom/Controller/CPNavigationController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+@class CPNavigationDelgate;
 
-@interface CPNavigationController : UINavigationController
+@interface CPNavigationController : UINavigationController {
+    CPNavigationDelgate *cpNaviDelegate;
+}
 
 @end
--- PosMini_Iphone/ui/Custom/Controller/CPNavigationController.m
+++ PosMini_Iphone/ui/Custom/Controller/CPNavigationController.m
@@ -7,12 +7,40 @@
 //
 
 #import "CPNavigationController.h"
+#import "PosMiniDevice.h"
+#import "BaseViewController.h"
+
+@interface CPNavigationDelgate : NSObject <UINavigationControllerDelegate>
+@end
+
+@implementation CPNavigationDelgate
+
+//防止popViewController时,设置错误的controller
+-(void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    BaseViewController *CTRL = (BaseViewController *) viewController;
+    [PosMiniDevice sharedInstance].baseCTRL = CTRL;
+}
+
+@end
 
 @interface CPNavigationController ()
 
 @end
 
 @implementation CPNavigationController
+
+-(void)dealloc{
+    [cpNaviDelegate release];
+    [super dealloc];
+}
+
+-(id)init{
+    self = [super init];
+    if (self != nil) {
+        cpNaviDelegate = [CPNavigationDelgate new];
+    }
+    return self;
+}
 
 //support ios6 for landscape mode
 - (NSUInteger)supportedInterfaceOrientations{
